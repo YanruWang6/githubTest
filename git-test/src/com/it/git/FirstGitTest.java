a3d244e284b8505de3356450fcbabe72f57ba3cf
tom_glb <1403164891@qq.com>
2020-10-06 17:31:54 +0800

    把本地工程添加到git

---
+++ git-test/src/com/it/git/FirstGitTest.java
@@ -0,0 +1,8 @@
+package com.it.git;
+
+public class FirstGitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
