13b5377a78a1eda7454cea78cd5e7eb4d6c14987
tom_glb <1403164891@qq.com>
2020-10-06 17:33:40 +0800

    修改测试

--- git-test/src/com/it/git/FirstGitTest.java
+++ git-test/src/com/it/git/FirstGitTest.java
@@ -4,5 +4,6 @@ public class FirstGitTest {
     public static void main(String[] args) {
         System.out.println("hello git project");
         System.out.println("hello git project");
+        System.out.println("添加");
     }
 }
